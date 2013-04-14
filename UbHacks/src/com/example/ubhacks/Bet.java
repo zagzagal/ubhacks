f940eade1c142aa27406997a1066cc18421fc7c5
Paul Schuster <pes5@buffalo.edu>
2013-04-14 10:29:11 -0400

    final for me/UBhacks 2013

---
+++ UbHacks/src/com/example/ubhacks/Bet.java
@@ -0,0 +1,13 @@
+package com.example.ubhacks;
+
+public class Bet {
+	int amount;
+	int time;
+	int hand;
+	
+	Bet(int amount, int time, int hand){
+		this.amount = amount;
+		this.time = time;
+		this.hand = hand;
+	}
+}
